a258cb95458beff0f4391e98a31ed994d895e1b8
Vidit Awasthi <viditawasthi2@gmail.com>
2021-03-28 21:07:21 +0530

    added Delete request

--- app.js
+++ app.js
@@ -49,6 +49,17 @@ app.post("/articles", function(req, res){
   });
 });
 
+app.delete("/articles", function(req,res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("Successfully Deleted All Articles.");
+    }
+    else{
+      res.send(err);
+    }
+  });
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
